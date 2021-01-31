f43c46653c2d098d74d7c6d1a46334b0350bbc4c
Ubuntu <nithinr07@RTOS-Linux.wzx5fzlisebezjv2au1x351joc.bx.internal.cloudapp.net>
2021-01-31 06:37:52 +0000

    added full-duplex server code

--- full-duplex/server.c
+++ full-duplex/server.c
@@ -14,7 +14,8 @@ int main(int argc, char const *argv[])
 	struct sockaddr_in address; 
 	int opt = 1; 
 	int addrlen = sizeof(address); 
-	char buffer[1024] = {0}; 
+	char read_buffer[1024] = {0}; 
+	char write_buffer[1024] ={0};
 	// char *hello = "Hello from server"; 
 	
 	// Creating socket file descriptor 
@@ -26,7 +27,7 @@ int main(int argc, char const *argv[])
 	
 	// Forcefully attaching socket to the port 8080 
 	if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, 
-												&opt, sizeof(opt))) 
+											&opt, sizeof(opt))) 
 	{ 
 		perror("setsockopt"); 
 		exit(EXIT_FAILURE); 
@@ -53,14 +54,26 @@ int main(int argc, char const *argv[])
 		perror("accept"); 
 		exit(EXIT_FAILURE); 
 	} 
-	while(1) {
-		memset(buffer, 0, sizeof(buffer));
-		valread = read(new_socket, buffer, 1024); 
-		if(strcmp(buffer, "bye") == 0) {
-			exit(0);
+	if(fork() == 0) {
+		while(1) {
+			memset(read_buffer, 0, sizeof(read_buffer));
+			valread = read(new_socket, read_buffer, 1024); 
+			printf("%s\n", read_buffer);
+			if(strcmp(read_buffer, "bye") == 0) {
+				exit(0);
+			}
+			}
+		} else {
+			while(1) {
+			memset(write_buffer, 0, sizeof(write_buffer));
+			scanf("%[^\n]%*c", write_buffer);
+			if(strcmp(write_buffer, "bye") == 0) {
+				exit(0);
+			}
+			send(new_socket, write_buffer, strlen(write_buffer), 0);
+			}
 		}
-		printf("%s\n", buffer);
-	} 
+		
 	// send(new_socket , hello , strlen(hello) , 0 ); 
 	// printf("Hello message sent\n"); 
 	return 0; 
