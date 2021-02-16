0378bc46d09214bc1a067686e1026d1d023fcc40
Nithin Raj <nithin.raj@iiitb.org>
2021-02-16 18:50:24 +0530

    Changed naming schemes

--- Group-Chat/include/message.h
+++ Group-Chat/include/message.h
@@ -3,6 +3,8 @@ struct Message {
     char name[100];
     int msgtype; // 0 for group, 1 for personal
     int recipient_id;
+    // int group_id;
+    // unsigned long long timestamp;
     char msg[1000];
 };
 
